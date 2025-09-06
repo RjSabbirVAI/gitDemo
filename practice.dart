75af07bed935d44d3aab9a7e93c9b2e619b97dec
RjSabbirVAI <srj1052@gmail.com>
2025-09-06 11:21:51 +0600

    condition statement if else.Dart

--- practice.dart
+++ practice.dart
@@ -1,9 +1,19 @@
-main(){
-  Map <String,int> studintname_roll={
-    "jahid" : 4834786,
-    "\ntamanna" : 5656788,
-    "\nshajid" : 6767665,
-  };
-  print(studintname_roll);
+ main(){
+  int a=7;
+  int b =7;
+  int c=7;
+  int d=10;
 
-}
+  if (a > b && a > c  &&  a > d)
+    print("a is larges");
+  else if (b > c && b > d  &&  b > a)
+    print("b is larges");
+  else if (c > d && c > a  &&  c > b)
+    print("c is larges");
+  else if (a==b && a==c &&  a==d)
+    print("All equal namber");
+  else
+   print ("d is larges");
+
+
+ }
